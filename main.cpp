fd5067d9f63098b3238a2f537d2f30d815e03b2e
Genomey <34148010+Gnomey123@users.noreply.github.com>
2024-01-22 17:22:03 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
+#define NOMINMAX
+#include <windows.h>
+
 #include <iostream>
+
+#include <unistd.h>
+
 
 #include "NEMA34new.h"
 
@@ -7,6 +13,19 @@
 int main()
 {
 
-	NEMA34 m1("COM3");
+	NEMA34 m1("\\\\.\\COM26");
+
+	m1.WRI(22, 80000000);	//set velocity
+
+	m1.WRI(21, 80000);		//set accell
+
+	m1.PVC(21,0,0,0);		//start motor
+
+
+
+	usleep(3000000);		//wait 3 sec
+
+
+	m1.STP(8000);			//stop motor
 
 }
